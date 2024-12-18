188246f7b042b23de184a1b12435443799355291
Anton Pukhno <pukhno.anton@gmail.com>
2024-12-18 12:31:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using AdventOfCode;
+
+var day1 = new Day1();
+day1.Part1();
+day1.Part2();
+
+var day2 = new Day2();
+day2.Part1();
+day2.Part2();
+
+var day3 = new Day3();
+day3.Part1();
+day3.Part2();
